952ff310e0ba751dd98bb4f2a9b2b38e224693ec
Mats Högberg <mats@hgbrg.se>
2016-10-20 21:49:03 +0200

    Bumped blk to version 0.2

--- blk.rb
+++ blk.rb
@@ -1,9 +1,9 @@
 class Blk < Formula
   desc "A simple bash script that makes it a breeze to block distracting websites"
   homepage "https://github.com/mhgbrg/blk"
-  url "https://github.com/mhgbrg/blk/archive/0.1.tar.gz"
-  version "0.1"
-  sha256 "861574a0f49000dd9d172504291307a43afb265dd340cab9a88bf032de9e3c49"
+  url "https://github.com/mhgbrg/blk/archive/0.2.tar.gz"
+  version "0.2"
+  sha256 "5fb294906c70c315e484ba72ea310cbe1dc8ab453e7cb48a77e90896d54561fa"
 
   def install
     bin.install "blk"
